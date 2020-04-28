2a93e4bdd2de84fb9754254fb607cf4a8e439437
LIZzARDsTeam <lizzardsteam@gmail.com>
2020-04-28 12:43:12 +0300

    Minor documentation fixes

--- README.md
+++ README.md
@@ -5,7 +5,8 @@ user input matches the registered combinations
 ## Usage
 To modify the registered key-binds:
 1. Open app.js
-2. Use the register method to add new key-binds
+2. Make sure to require the model
+3. Use the register method to add new key-binds
 ```js
 let binder = new KeyBindListener();
 binder.register("SHIFT", "E", () => alert("Activated key-bind: Shift + E"));
--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Initialize the listener
 let binder = new KeyBindListener();
 
-// Use the register method to add new keybindings and actions to the listener
+// Use the register method to add new key-bindings and actions to the listener
 binder.register("SHIFT", "Q", () => alert("Activated key-bind: Shift + Q"));
 binder.register("SHIFT", "E", () => alert("Activated key-bind: Shift + E"));
